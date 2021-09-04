bf79a1c617456f5d5a402e4b473edf04916e8cc3
Federico Palma <fedepal9@gmail.com>
2021-09-04 17:46:08 +0200

    Console game finished.

--- script.js
+++ script.js
@@ -20,25 +20,59 @@ function computerPlay() {
 function playerPlay() {
     while (true) {
         let playerChoice = prompt("Choose Rock, Paper or Scissors:");
+
+        if (playerChoice == null) {
+            alert("CAGON")
+        }
         playerChoice = playerChoice.charAt(0).toUpperCase() + playerChoice.toLowerCase().slice(1);
 
         if (playerChoice == "Rock" || playerChoice == "Paper" || playerChoice == "Scissors") {
             return playerChoice;
         } else {
-            alert("Please choose a valid option.")
+            alert("Please choose a valid option.");
         }
     }
 }
 
 function playRound(playerSelection, computerSelection) {
-    console.log(playerSelection)
-    console.log(computerSelection)
-
     if (playerSelection === "Rock" && computerSelection === "Scissors" || playerSelection === "Paper" && computerSelection === "Rock"|| playerSelection === "Scissors" && computerSelection === "Paper") {
-        return `Player chooses ${playerSelection}, Computer chooses ${computerSelection}, Player wins!!!`;
+        return [`Player chooses ${playerSelection}, Computer chooses ${computerSelection}, Player wins!!!`, 0]; 
     }else if (computerSelection === playerSelection) {
-        return `Player chooses ${playerSelection}, Computer chooses ${computerSelection}, It's a tie!!!`;
+        return [`Player chooses ${playerSelection}, Computer chooses ${computerSelection}, It's a tie!!!`, 1];
     }else{
-        return `Player chooses ${playerSelection}, Computer chooses ${computerSelection}, Computer wins!!!`;
+        return [`Player chooses ${playerSelection}, Computer chooses ${computerSelection}, Computer wins!!!`, 2];
     }
+}
+
+function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+    let goal = 5
+
+    while (playerScore < goal && computerScore < goal) {
+        console.log(`Player ${playerScore} points, Computer ${computerScore} points.`)
+
+        let roundResult = playRound(playerPlay(), computerPlay())
+        let roundText = roundResult[0]
+        let roundCode = roundResult[1]
+
+        console.log(roundText);
+
+        if (roundCode === 0) {
+            console.log("1 point for Player!")
+            playerScore++;
+        } else if (roundCode === 1) {
+            console.log("No points for anybody.")
+        } else {
+            computerScore++
+            console.log("1 point for Computer!")
+        }
+    }
+
+    if (playerScore > computerScore) {
+        console.log("Player wins the game!")
+    } else {
+        console.log("Computer wins the game!")
+    }
+    
 }
